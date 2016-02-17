3d6db57ee41897258719e6118683bf3208f310cb
Paul Wessel <pwessel@hawaii.edu>
2016-02-17 18:22:38 +0000

    update search

--- guru/function_search.sh
+++ guru/function_search.sh
@@ -5,6 +5,10 @@
 # Plan is to set #define LOCAL static and use things like
 # LOCAL double somelocalfunc (...) to set the scope
 # List of C files
+if [ ! -d src ]; then
+	echo "Run from trunk"
+	exit
+fi
 find src -name '*.c' > t.lis
 rm -rf log
 mkdir -p log
@@ -13,8 +17,13 @@ while read file; do # For each C file
 	# Determine all the functions in this file
 	log=log/`basename $file ".c"`.lis
 	echo "Create $log"
-	egrep '{$' $file | egrep '^bool |^int |^float |^double |^char |^uint64_t |^int64_t |^void ' | tr '(*' '  ' | awk '{if ($2 == "*") {print $3} else {print $2}}' > $log
-	egrep '{$' $file | egrep '^enum ^short |^unsigned |^struct ' | tr '(*' '  ' | awk '{if ($3 == "*") {print $4} else {print $3}}' >> $log
+	egrep '{$' $file | egrep '^bool |^int |^float |^double |^char |^uint64_t |^int64_t |^void ' | tr '(*' '  ' | awk '{if ($2 == "*") {print $3} else {print $2}}' > tmp
+	egrep '{$' $file | egrep '^enum ^short |^unsigned |^struct ' | tr '(*' '  ' | awk '{if ($3 == "*") {print $4} else {print $3}}' >> tmp
+	# Eliminate struct and array declarations and main functions
+	egrep -v '\[|\]|\{|\}|^main' tmp > $log
+	if [ ! -s $log ]; then
+		rm -f $log
+	fi
 	# See where else these functions are being used
 done < t.lis
-rm -f t.lis
+rm -f t.lis tmp
