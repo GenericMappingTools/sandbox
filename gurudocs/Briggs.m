b532242b8841b99365de14a20c658bcdd19493df
Paul Wessel <pwessel@hawaii.edu>
2016-04-12 08:17:29 +0000

    Update and rename Briggs.*

--- gurudocs/Briggs.m
+++ gurudocs/Briggs.m
@@ -1,21 +1,22 @@
-% Solving for surface Briggs coefficients for the 4 quadrants.
+% $Id$
+% Solving for surface's Briggs coefficients for the 4 quadrants.
 % P. Wessel, April 2016.
 %
 % We will do the 4 quadrants by specifying the data constraint's
 % relative location as x,y but include the signs given the quadrant.
-% I.e., for quadrant 2 the point is -x, +y.  In other words,
+% I.e., for quadrant 2 the point is (-x, +y).  In other words,
 % x and y are the absolute values of the offsets.
 % However, when the aspect ratio is not one then we need to
 % use y = alpha * y in the expressions.  So we use y and x
-% to test if we are close to a node (to fix it) but x, alphy
+% to test if we are close to a node (to fix it) but x, alpha * y
 % in the expressions below.
 
-syms x y A rhs
+syms x y A rhs alpha
 rhs = [0
        0
        2
        0
-       2];
+       2*alpha^2];
 % point is in quadrant 1, so use nodes 1 5 9 10
 % Point E has coordinates +x,+y
 %node: 1  5  9 10  E
@@ -53,9 +54,11 @@ A = [  1  0 -1 -1  x
        1  1  0  1  y^2];
 bq4 = A \ rhs
 
-% Compare 1st and 2nd
+% Compare 1st and 2nd and find they are identical
 simplify(bq2-bq1)
-% Compare 3rd and 4th
+% Compare 3rd and 4th and find they are identical
 simplify(bq4-bq3)
-% Compare 1st and 3rd
+% Compare 1st and 3rd and find they differ
 simplify(bq3-bq1)
+% Compare 2nd and 4th and find they differ
+simplify(bq4-bq2)
