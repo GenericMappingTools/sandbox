5f29347cc154e9e89c98af25cb26dcf40dc32007
Paul Wessel <pwessel@hawaii.edu>
2016-04-12 21:12:26 +0000

    Add test to the Matlab code

--- gurudocs/Briggs.m
+++ gurudocs/Briggs.m
@@ -62,3 +62,36 @@ simplify(bq4-bq3)
 simplify(bq3-bq1)
 % Compare 2nd and 4th and find they differ
 simplify(bq4-bq2)
+
+% Now test the b_k's for some arbitrary point and aspect ratio
+
+uE = 0.45;  vE = 0.05;  alpha = 1;
+D = (uE + vE + 1)*(uE + vE);
+bnew = [(alpha^2*(uE^2 + 2*uE*vE + uE) - (vE^2 + vE))/D
+                                       (2*(vE - alpha^2*uE + 1))*(uE + vE)/D
+                                       (2*(alpha^2*uE - vE + alpha^2))*(uE + vE)/D
+ (vE^2 + vE + 2*uE*vE - alpha^2*(uE^2 + uE))/D
+                                   (2*(alpha^2 + 1))/D]
+
+% Original coefficients given in Geophysics paper seems partly wrong (b2)
+b5 = (2*(alpha^2 + 1))/((uE+vE)*(1+uE+vE));
+b4 = 1 - 0.5*b5*(uE+uE^2);
+b3 = vE*(1+uE)*b5-2*b4;
+b2 = b3 - (uE^2-vE^2)*b5;
+%b2 = vE*(1+uE)*b5-b3; <-- This is original paper eq but this must be wrong
+b1 = uE*b5 + b4 - b2; % <-- this seems correct
+bpaper = [b1; b2; b3; b4; b5]
+
+% Coded up from surface.c matches bnew when alpha == 1
+dxpdy = uE + vE;
+xys = 1.0 + dxpdy;
+btemp = 2*(alpha^2 + 1) / ( dxpdy * xys );
+b1 = 1.0 - 0.5 * (uE + (uE * uE)) * btemp;
+b4 = 0.5 * (2*alpha^2 - (vE + (vE * vE)) * btemp);
+xy1 = 1.0 / xys;
+b2 = (2*alpha^2 * xys - 4 * vE) * xy1;
+b3 = 2 * (vE - uE + 1.0) * xy1;
+b5 = btemp;
+b6 = b1 + b2 + b3 + b4 + b5;
+
+code = [b1; b2; b3; b4; b5]
