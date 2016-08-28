043bb78369f3321b96f7ff2ea0d4fe2bf89059d2
Paul Wessel <pwessel@hawaii.edu>
2016-08-28 23:21:12 +0000

    Rename ColorInterp to color_interp in struct GMT_IMAGE for consistency in variable names

--- format_svnlog.sh
+++ format_svnlog.sh
@@ -5,4 +5,4 @@
 #
 # format_log.sh 2013-11-08
 now=`date -u +"%Y-%m-%d"`
-svn log -r {$now}:{2013-11-08} | egrep -v '\-\-\-|^$' | awk '{if ($2 =="|" && $4 == "|") {printf "\n%s\t%s\n", $5, $3} else {printf "\t* filename.ext:\t\t%s\n", $0}}'
+svn log -r {$now}:{2015-11-12} | egrep -v '\-\-\-|^$' | awk '{if ($2 =="|" && $4 == "|") {printf "\n%s\t%s\n", $5, $3} else {printf "\t* filename.ext:\t\t%s\n", $0}}'
