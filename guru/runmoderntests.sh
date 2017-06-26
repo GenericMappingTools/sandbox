03a7b20e8d77e866cc780adc5655191fa0674a4e
Paul Wessel <pwessel@hawaii.edu>
2017-06-26 23:05:56 +0000

    Place modern test script in guru folder

---
+++ guru/runmoderntests.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+cd test
+ls -d modern_* > d.lis
+while read dir; do
+	cd $dir
+	ls *.sh > s.lis
+	while read script; do
+		base=`basename $script '.sh'`
+		echo "Running $script"
+		if [ -f $base.ps ]; then
+			mv $base.ps m_${base}.ps
+		fi
+		rm -f gmt.conf gmt.history
+		cat <<- EOF > gmtest.sh
+		#!/bin/bash
+		export GMT_SOURCE_DIR="$GMTHOME5/trunk"
+		# Start with proper GMT defaults
+		gmt set -Du
+		# Now run the original script
+		. $script
+		rm -f gmt.conf gmt.history
+		EOF
+		bash gmtest.sh
+	done < s.lis
+	rm -f s.lis
+	cd ..
+done < d.lis
+rm -f d.lis
+cd ..
