2dbe88db553814ceac29dea20b7ff9fcf3a239a6
Paul Wessel <pwessel@hawaii.edu>
2017-08-06 05:39:25 +0000

    Fix colorbar

--- casanova/spheredist.sh
+++ casanova/spheredist.sh
@@ -2,7 +2,7 @@
 # Determine the distances from every grid node to the nearest coastline
 # provided by the coastline segments in gshhs_c.txt.
 # We evaluate the grid with distances using sphdistance.
-# This is very slow (5 minutes on my laptop).  The result
+# This is pretty slow (5 minutes on my laptop).  The result
 # is a PNG file (spheredist.png) @ 300 dpi.
 
 verbose=-V	# Comment this out for a quiet run
@@ -16,7 +16,8 @@ gmt sphdistance -Rg -I2m -Qtt.pol -Gtt.nc -Lk $verbose
 gmt makecpt -Chot -T0/3500 > t.cpt
 # Make a basic image plot and overlay contours, Voronoi polygons and coastlines
 gmt grdimage tt.nc -JG-150/30/7i -P -K -Ct.cpt -X0.75i -Y2i > $ps
-gmt pscoast -R -J -O -W1p -Dh -Gsteelblue -A0/1/1 -B30g30 >> $ps
+gmt pscoast -R -J -O -W1p -Dh -Gsteelblue -A0/1/1 -B30g30 -K >> $ps
+gmt psscale -Ct.cpt -O -Dx3.5i/7.5i+w6i+h+jBC -Baf >> $ps
 # Make png
 gmt psconvert $ps -TG -A -P -E300 -Z
 # Clean up
