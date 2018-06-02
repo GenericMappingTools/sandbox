dbfb88ea7a7e4754afe1445a2187d15f65c0d2f2
Paul Wessel <pwessel@hawaii.edu>
2018-06-02 05:59:48 +0000

    Update script

--- gurudocs/polygon_wrap_figs.sh
+++ gurudocs/polygon_wrap_figs.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 # This script makes the figures used in PolygonWrapping.tex
 # Paul Wessel, May 30, 2018
-# It uses the two files test_polygon_lonlat.txt and test_polygon_xy_J[MH].txt
-#
+# It uses the the files test_polygon_lonlat.txt and test_polygon_xy_J[MH].txt
+# test_polar_lonlat.txt and test_polar_xy_JN.txt
 # test_polygon_xy_J[MH].txt were produced by running 
-# gmt psxy -R0/360/0/90 -JH180/6i -P -Baf test_polygon_lonlat.txt -Gred > o.ps
-# gmt psxy -R180/180/15/65 -JM0/6i -P -Baf test_polygon_lonlat.txt -Gred > o.ps
+# gmt psxy -R0/360/0/90 -JH180/6i test_polygon_lonlat.txt -Gred -A > /dev/null
+# gmt psxy -R180/180/15/65 -JM0/6i test_polygon_lonlat.txt -Gred -A > /dev/null
+# gmt psxy -R-180/180/-90/-55 -JN-70/6i test_polar_lonlat.txt -Gred -A > /dev/null
 # and doctoring the code to write out test_polygon_xy_J[HM].txt as
 # the projected x,y in inches but AFTER we have determined all
 # the E-W jumps and added extra points at both the exit and reentry
@@ -30,7 +31,7 @@ gmt psxy -R-0.5/6.75/0/1.5 test_polygon_xy_JM.txt -Jx1i -P -Wfaint -K > pwrap_JM
 gmt psxy -R test_polygon_xy_JM.txt -J -O -K -Sc1p -Gred >> pwrap_JM_2.ps
 gmt psxy -R0/360/15/65 -JM180/6i -O -T -X0.5i -B0 --MAP_FRAME_PEN=faint --MAP_FRAME_TYPE=plain >> pwrap_JM_2.ps
 
-# Create the left-warpped and right-wrapped polygons
+# Create the left-wrapped and right-wrapped polygons
 awk '{printf "%.16g\t%s\n", $1+(($3-1)%2)*$4, $2}' test_polygon_xy_JM.txt > R.txt
 awk '{printf "%.16g\t%s\n", $1-(($3)%2)*$4, $2}'   test_polygon_xy_JM.txt > L.txt
 
@@ -56,7 +57,7 @@ gmt psxy -R-0.5/6.75/0/1.5 test_polygon_xy_JH.txt -Jx1i -P -Wfaint -K > pwrap_JH
 gmt psxy -R test_polygon_xy_JH.txt -J -O -K -Sc1p -Gred >> pwrap_JH_2.ps
 gmt psxy -R0/360/0/90 -JH180/6i -O -T -X0.5i -B0 --MAP_FRAME_PEN=faint >> pwrap_JH_2.ps
 
-# Create the left-warpped and right-wrapped polygons
+# Create the left-wrapped and right-wrapped polygons
 awk '{printf "%.16g\t%s\n", $1+(($3-1)%2)*$4, $2}' test_polygon_xy_JH.txt > R.txt
 awk '{printf "%.16g\t%s\n", $1-(($3)%2)*$4, $2}'   test_polygon_xy_JH.txt > L.txt
 
@@ -68,5 +69,17 @@ gmt psxy -R0/360/0/90 -JH180/6i -O -T -X0.5i -B0 --MAP_FRAME_PEN=faint >> pwrap_
 gmt psxy -R-0.5/6.75/0/1.5 L.txt R.txt -Jx1i -P -Wfaint -K > pwrap_JH_4.ps
 gmt psxy -R0/360/0/90 -JH180/6i -O -Gred -X0.5i test_polygon_lonlat.txt -B0 --MAP_FRAME_PEN=faint >> pwrap_JH_4.ps
 
-gmt psconvert pwrap_JH_?.ps -Tf -P -A -Z
+# Polar caps
+gmt psxy -R-180/180/-90/-55 -JA-70/-90/1i -Gred -W1p -P -Bafg test_polar_lonlat.txt -K --MAP_FRAME_TYPE=plain --MAP_FRAME_PEN=faint > pwrap_JN_1.ps
+gmt psxy -R -J -O -A -W1p << EOF >> pwrap_JN_1.ps
+110	-90
+110	-55
+EOF
+
+# Raw plot of wrapped polygon
+gmt psxy -R0/6/0/0.8 -Jx1i -Wfaint test_polar_xy_JN.txt -P -K --MAP_FRAME_PEN=faint > pwrap_JN_2.ps
+gmt psxy -R test_polar_xy_JN.txt -J -O -K -Sc2p -Gred >> pwrap_JN_2.ps
+gmt psxy -R-180/180/-90/-55 -JN-70/6i -B0 -O -T --MAP_FRAME_PEN=faint >> pwrap_JN_2.ps
+
+gmt psconvert pwrap_J?_?.ps -Tf -P -A+m3p -Z
 rm -f L.txt R.txt
