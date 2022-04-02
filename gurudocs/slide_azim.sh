df2c05b8c2c949bc6fa1fa95b82ba4f9ddd5095f
Paul Wessel <pwessel@hawaii.edu>
2022-04-02 17:52:56 +0100

    Update notes

--- gurudocs/slide_azim.sh
+++ gurudocs/slide_azim.sh
@@ -2,12 +2,16 @@
 # Makes fig3 in slides.tex
 gmt begin slides_fig3 png
 	gmt basemap -R-1/1/0/1 -JX4i/1i -Bxaf+l"@~g@~" -Byaf+l"s"
-	v=2;
+	v=2;	s_bar=$(gmt math -Q 1 1 $v 1 ADD DIV SUB =)
 	gmt math -T-1/1/0.001 T ABS $v POW = | gmt plot -W1p,red -l"@%6%p = 2@%%"+jTC
-	v=5
+	gmt math -T-1/1/2 $s_bar = | gmt plot -W0.5p,red,-
+	v=5;	s_bar=$(gmt math -Q 1 1 $v 1 ADD DIV SUB =)
 	gmt math -T-1/1/0.001 T ABS $v POW = | gmt plot -W1p,green -l"@%6%p = 5@%%"
-	v=10
+	gmt math -T-1/1/2 $s_bar = | gmt plot -W0.5p,green,-
+	v=10;	s_bar=$(gmt math -Q 1 1 $v 1 ADD DIV SUB =)
 	gmt math -T-1/1/0.001 T ABS $v POW = | gmt plot -W1p,blue -l"@%6%p = 10@%%"
-	v=100
+	gmt math -T-1/1/2 $s_bar = | gmt plot -W0.5p,blue,-
+	v=100;	s_bar=$(gmt math -Q 1 1 $v 1 ADD DIV SUB =)
 	gmt math -T-1/1/0.001 T ABS $v POW = | gmt plot -W1p,black -l"@%6%p = 100@%%"
+	gmt math -T-1/1/2 $s_bar = | gmt plot -W0.5p,black,-
 gmt end show
