054d3e0f0bc36fd2a6b8ffa14cb6d610c7d76bc1
Paul Wessel <pwessel@hawaii.edu>
2022-04-10 16:35:49 +0100

    Workn on u- and phi

---
+++ gurudocs/messaround.m
@@ -0,0 +1,34 @@
+function messaround
+r1 = 46.785714285714285; h1 = 500;
+r2 = 11.428571428571427;    h2 = 6000;
+rc = 46.785714285714285;  hc = 500;
+r0 = 50;    h0 = 7000;
+rd = 94.647438856464958;
+a1 = -20;   a2 = 130; theta = (a2 - a1)/360
+f = 0.1;
+u0 = 0.05;
+V0 = pi * r0 * r0 * h0 * (1 - f^3)/(3*(1-f))
+% COmpute flank volume
+u1 = r1/r0; u2 = r2/r0;
+K = u1 - u2 - 0.5 * (u1^2 - u2^2)
+uf = (3 * (u1^2 - u2^2) - 2.0 * (u1^3 - u2^3))/(6*K)
+Af = h0 * r0 * K / (1 - f)
+rf = r0 * uf
+Vf = 2 * pi * Af * rf
+% Aql
+dr = r1 - r2;   dh = h2 - h1;
+rql = 0.5 * (r1 + r2)
+Aql = dr * h1
+% Aqu
+Aqu = dh * dr * u0 * ((1 + u0) * log ((1 + u0)/u0) - 1)
+uqu = ((1 + u0)* (1 - u0 * log ((1 + u0)/u0)) - 0.5) / ((1 + u0) * log((1 + u0)/u0) - 1)
+rqu = r2 + dr * uqu
+Vq = 2 * pi * (Aql * rql + Aqu * rqu)
+Vs_0 = (Vf - Vq) * theta
+phi = 100 * Vs_0 / V0
+
+% Now use this phi to solve for u0 etc.
+
+Vs = phi * V0 / (100*theta)    % Desired volume of slide scaled to 360
+rhs = ((Vf - Vs)/(pi*dr) - h1 * (r1 + r2))/(2*dh)
+u0 = solve_for_u0 (r1, r2, h1, h2, rhs);
---
+++ gurudocs/solve_for_u0.m
@@ -0,0 +1,26 @@
+function u0 = solve_for_u0 (r1, r2, h1, h2, rhs)
+dr = r1 - r2; dh = h2 - h1;
+prev_u0 = 0.1; w = 2.0;	% Trial value 0.5, use over-relaxation of 2
+n_iter = 0;
+MAX_ITERATIONS=1000;
+GMT_CONV12_LIMIT=1e-12;
+go = 1;
+while (go == 1)
+	u0_1 = 1.0 + prev_u0;
+	u0 = rhs / ((r2 + dr * ubar (prev_u0)) * (u0_1 * log (u0_1 / prev_u0) - 1.0));
+	du = abs (prev_u0 - u0);
+	%fprintf (stderr, "%d prev_u0 = %lg u0 = %lg du = %lg\n", n_iter, prev_u0, u0, du);
+	prev_u0 = w * u0 + (1.0 - w) * prev_u0;
+	n_iter = n_iter + 1;
+	if (du > GMT_CONV12_LIMIT && n_iter < MAX_ITERATIONS)
+        go = 0;
+    end
+end
+disp (['Iteration = ' int2str(n_iter)])
+end
+
+function ub = ubar (u0)
+u0_1 = 1.0 + u0;
+ub = (u0_1 * (1.0 - u0 * log (u0_1 / u0)) - 0.5) / (u0_1 * log (u0_1 / u0) - 1.0);
+end
+
