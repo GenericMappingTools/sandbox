465bc3eff3c93650be798738e6d6724b6dd72fb3
Max Jones <meghanj@alum.mit.edu>
2022-08-27 09:36:20 -0400

    Use logo colors for text in ecosystem plot

--- guru/gmt-eco-system.sh
+++ guru/gmt-eco-system.sh
@@ -20,9 +20,9 @@ gmt begin gmt-eco-system png
 	# Place logo without label
 	gmt logo -DjCT+w1.5i+o0/0.05i -Sn
 	# Add label and purpose
-	echo " 0 0.25 THE GMT ECOSYSTEM" | gmt text -F+f12p,Helvetica-Bold
-	echo " 0 0.10 generic-mapping-tools.org" | gmt text -F+f6p,Helvetica
-	echo " 0 -0.01 Data Processing \035 Plots \035 Animations" | gmt text -F+f8.5p,Times-Italic
+	echo " 0 0.25 THE GMT ECOSYSTEM" | gmt text -F+f12p,Helvetica-Bold,92/102/132
+	echo " 0 0.10 generic-mapping-tools.org" | gmt text -F+f6p,Helvetica,92/102/132
+	echo " 0 -0.01 Data Processing \035 Plots \035 Animations" | gmt text -F+f8.5p,Times-Italic,92/102/132
 	# Separating line
 	gmt plot -W0.5p <<- EOF
 	-0.95	-0.12
@@ -31,7 +31,7 @@ gmt begin gmt-eco-system png
 	# Place QR code
 	echo 0 -0.4 | gmt plot -SkQR/0.55i
 	# Ecosystem identifiers
-	gmt text -F+f9p,Helvetica-Bold,darkgreen+jCM <<- EOF
+	gmt text -F+f9p,Helvetica-Bold,238/86/52+jCM <<- EOF
 	-0.65 -0.22 CMD
 	-0.65 -0.4 JULIA
 	-0.65 -0.58 MATLAB
@@ -40,7 +40,7 @@ gmt begin gmt-eco-system png
 	0.65 -0.58 OCTAVE
 	EOF
 	# OS supported
-	echo "Linux \031 macOS \031 Windows" | gmt text -F+cBC+f8p,Helvetica-Bold,navy -Dj0/0.18i
+	echo "Linux \031 macOS \031 Windows" | gmt text -F+cBC+f8p,Helvetica-Bold,125/168/125 -Dj0/0.18i
 	# Funding
-	echo "SUPPORTED BY THE US NATIONAL SCIENCE FOUNDATION" | gmt text -F+cBC+f4.75p,Helvetica-Bold -Dj0/0.05i
+	echo "SUPPORTED BY THE US NATIONAL SCIENCE FOUNDATION" | gmt text -F+cBC+f4.75p,Helvetica-Bold,92/102/132 -Dj0/0.05i
 gmt end show
